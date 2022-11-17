4528b2d92ce6b2d463b6c3c3fa42683263d96097
Leland Capps <lelandcapps@Lelands-MacBook-Air.local>
2022-11-17 15:57:12 -0500

    countdown timer with JS

---
+++ countdown timer/js/logic.js
@@ -0,0 +1,15 @@
+let day = document.getElementById('days')
+let hours = document.getElementById('hours')
+let min = document.getElementById('min')
+let sec = document.getElementById('sec')
+let startTime 
+
+
+
+
+let updateTime = (d, h, m, s) =>{
+
+    console.log(d.textContent, h.textContent, m.textContent, s.textContent)
+}
+
+updateTime(day, hours, min, sec)
